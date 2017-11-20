7c97895c2f267a67e34e98acffe6bc44743dbca2
linzhang0x <linzhang0x@gmail.com>
2017-11-20 22:17:25 +0800

    move & mode print

--- curses-demo/demo.cpp
+++ curses-demo/demo.cpp
@@ -1,24 +1,61 @@
 #include <iostream>
 #include <curses.h>
 
+
+void printyx(const char *str, int y, int x);
+
 int main() {
 	initscr();
-
-	// WINDOW *win = newwin(10, 12, 0, 0); 
 	int ch;
-	// nodelay(stdscr, TRUE);
+	int row;
+	int col;
 	noecho();
+	//raw();
+	//keypad(stdscr, true);
+	getmaxyx(stdscr, row, col);
+	printyx("NORMAL", row - 1, 0);
+	// 2 是整个
+	// 5 是半个
+	curs_set(2);
 	for (;;) {
 		if ((ch = getch()) == ERR) {
-			addch('a');
-		} else {
-			if (ch == KEY_ENTER) {
-				move(1, 0);
-				refresh();
-			} else {
-				addch(ch | A_UNDERLINE);
+			printyx("ERR", row - 1, 0);
+		}
+		else {
+			int cury = getcury(curscr);
+			int curx = getcurx(curscr);
+			switch (ch)
+			{
+			case 'h':
+				move(cury, curx - 1);
+				break;
+			case 'j':
+				move(cury + 1, curx);
+				break;
+			case 'k':
+				move(cury - 1, curx);
+				break;
+			case 'l':
+				move(cury, curx + 1);
+				break;
+			default:
+				break;
 			}
-			// std::cerr << ch << std::endl;
+			refresh();
+			//printyx("w", cury, curx);
+			cury = getcury(curscr);
+			curx = getcurx(curscr);
+			addch('A' | A_BOLD);
+			move(cury, curx);
+			continue;
 		}
 	}
+}
+
+void printyx(const char *str, int y, int x) {
+	int curx = getcurx(stdscr);
+	int cury = getcury(stdscr);
+	move(y, x);
+	printw(str);
+	move(cury, curx);
 }
